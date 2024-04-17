d609b958d063e4827a030152674ab871d56a4c51
N-S-N <135265220+N-S-N@users.noreply.github.com>
2024-04-17 08:31:31 -0300

    dia de apresentação

--- Hardspace factorio/Assets/Script/Buld System/ObjectPlacer.cs
+++ Hardspace factorio/Assets/Script/Buld System/ObjectPlacer.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.Drawing;
 using UnityEngine;
-using static UnityEditor.FilePathAttribute;
+
 
 public class ObjectPlacer : MonoBehaviour
 {
