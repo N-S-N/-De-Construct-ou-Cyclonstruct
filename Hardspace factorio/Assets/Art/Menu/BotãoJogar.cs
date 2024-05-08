f51712e3a389cb3ae1d2705f83eb77e4e70b0b01
Carlos-EduAG <carlos.e.garcia@iesb.edu.br>
2024-05-08 17:57:20 -0300

    Botão Jogar Funcionando

---
+++ Hardspace factorio/Assets/Art/Menu/BotãoJogar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Control : MonoBehaviour
+{
+    public void CenaTestAndBuildSystem()
+    {
+        SceneManager.LoadScene("Test and Buld System");
+    }
+}
