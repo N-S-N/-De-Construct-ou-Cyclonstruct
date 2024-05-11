878101a9a8b741e111ded1068a2800ca5968bf42
N-S-N <135265220+N-S-N@users.noreply.github.com>
2024-05-11 14:00:04 -0300

    catsine já implementada

--- Hardspace factorio/Assets/Art/Menu/BotãoJogar.cs
+++ Hardspace factorio/Assets/Art/Menu/BotãoJogar.cs
@@ -1,11 +1,31 @@
-using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class Control : MonoBehaviour
 {
-    public void CenaTestAndBuildSystem()
+    [SerializeField] VideoPlayer vidio;
+    private int sala = -1;
+    public void CenaTestAndBuildSystem(int salaLOad)
     {
-        SceneManager.LoadScene("Test and Buld System");
+        //SceneManager.LoadScene("Test and Buld System");
+        if (salaLOad+1 == 2)
+        {
+            SceneManager.LoadSceneAsync(salaLOad);
+            return;
+        }
+
+        vidio.Play();
+        sala = salaLOad;
+        float timevidio = vidio.clip.frameCount/ (float)vidio.clip.frameRate;
+       // Debug.Log(timevidio);
+        Invoke("LoadSene",timevidio);
+    }
+
+    void LoadSene()
+    {
+        SceneManager.LoadSceneAsync(sala);
     }
 }
